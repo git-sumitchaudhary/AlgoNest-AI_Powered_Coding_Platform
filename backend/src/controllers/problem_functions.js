1df7f37000f69be2eaa18037ef4eec4b082706b9
git-sumitchaudhary <155057360+git-sumitchaudhary@users.noreply.github.com>
2026-03-06 00:38:30 +0530

    model update f

--- backend/src/controllers/problem_functions.js
+++ backend/src/controllers/problem_functions.js
@@ -362,7 +362,7 @@ const ai_chat = async (req, res) => {
 
         async function main() {
             const response = await ai.models.generateContent({
-                model: "gemini-2.0-flash",
+                model: "gemini-2.5-flash",
                 contents: messages,
                 config: {
                     systemInstruction: `
