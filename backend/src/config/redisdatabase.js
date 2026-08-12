f2b8f4b559f065237724851d4e4863dcded03fc0
git-sumitchaudhary <155057360+git-sumitchaudhary@users.noreply.github.com>
2026-08-12 19:45:53 +0530

    Update redisdatabase.js

--- backend/src/config/redisdatabase.js
+++ backend/src/config/redisdatabase.js
@@ -4,11 +4,10 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-11787.crce206.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 11787
+        host: 'redis-17474.crce281.ap-south-1-3.ec2.cloud.redislabs.com',
+        port: 17474
     }
 });
-
 
 module.exports = redisClient;
 
